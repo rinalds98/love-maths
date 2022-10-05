8d96f218b25cd88ebde15f220ec7e183bf48d378
rinalds98 <rinalds98@gmail.com>
2022-10-05 21:40:28 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,25 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+
 })
 /**
  * The main game "loop", called when script first loaded
  */
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() *25 + 1);
     let num2 = Math.floor(Math.random() *25 + 1);
 
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    } else {`Unknown game type: ${gameType}`
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    };
 }
 
 function checkAnswer(){
@@ -37,8 +44,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion(){
