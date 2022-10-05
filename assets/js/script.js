76fe874faffdab67b5ae8284d17134bca5ef5760
rinalds98 <rinalds98@gmail.com>
2022-10-05 22:47:07 +0000

    Add Favicon, and final tisy ups for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,12 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         });
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+    })
     runGame("addition");
 
 })
@@ -18,6 +24,10 @@ document.addEventListener("DOMContentLoaded", function(){
  * The main game "loop", called when script first loaded
  */
 function runGame(gameType){
+    
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() *25 + 1);
     let num2 = Math.floor(Math.random() *25 + 1);
 
