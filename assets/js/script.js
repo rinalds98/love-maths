cefd00ec40b5afc282491ce5792d49d751667b50
rinalds98 <rinalds98@gmail.com>
2022-10-05 23:35:42 +0000

    Add division operator to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,9 @@ function runGame(gameType){
     else if(gameType === "multiply"){
         displayMultiplyQuestion(num1,num2);
     } 
-    
+    else if (gameType === "division"){
+        displayDivisionQuestion(num1,num2);
+    }
     else{`Unknown game type: ${gameType}`
         throw `Unknown game type: ${gameType}. Aborting!`;
     };
@@ -76,7 +78,9 @@ function calculateCorrectAnswer(){
     else if ( operator === "x"){
         return[operand1 * operand2, "multiply"];
     }
-
+    else if (operator === "/"){
+        return[operand1 / operand2, "division"]
+    }
     else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -114,4 +118,11 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
